f26300eec1ede4dddbd38c70443be33604adbeba
SidLin <j611062000@gmail.com>
2018-06-08 17:13:08 +0800

    merge_sort

---
+++ merge_sort.py
@@ -0,0 +1,42 @@
+def isIndexSafe(index, array):
+    if index <= len(array)-1:
+        return True
+    elif index > len(array)-1:
+        return False
+
+def mergesort(array):
+
+    if len(array) <= 1:
+        return array
+
+    l=len(array)//2
+    Larray=mergesort(array[:l])
+    Rarray=mergesort(array[l:])
+
+    temp=[]
+    i=-1
+    j=-1
+
+    while isIndexSafe(i+1,Larray) or isIndexSafe(j+1,Rarray):
+
+        if isIndexSafe(i+1,Larray) and isIndexSafe(j+1,Rarray):
+            if Larray[i+1]>Rarray[j+1]:
+                temp.append(Rarray[j+1])
+                j+=1
+            else:
+                temp.append(Larray[i+1])
+                i+=1
+
+        elif isIndexSafe(i+1,Larray):
+            temp.append(Larray[i+1])
+            i+=1
+
+        elif isIndexSafe(j+1,Rarray):
+            temp.append(Rarray[j+1])
+            j+=1
+
+    return temp
+
+print(mergesort([5,1,0,4,6,8,7]))
+
+
