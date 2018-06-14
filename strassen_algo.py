73febeae7a0b30047dde9a915c7b0e75f2502e9c
SidLin <j611062000@gmail.com>
2018-06-14 17:29:51 +0800

    revising strassen's algorithm

--- strassen_algo.py
+++ strassen_algo.py
@@ -1,18 +1,3 @@
-def concatenate(lu,ld,ru,rd):
-    result = []
-    row_u = len(lu)
-    row_d = len(ld)
-
-    for i in range(row_u):
-        row_temp = lu[i] + ru[i]
-        result.append(row_temp)
-
-    for j in range(row_d):
-        row_temp = ld[j] + rd[j]
-        result.append(row_temp)
-
-    return result
-
 def naive_multiplication(matrixA, matrixB):
     """
     matrixA: a m x n matrix
@@ -39,6 +24,21 @@ def naive_multiplication(matrixA, matrixB):
             result.append(row_temp)
 
         return result
+        
+def concatenate(lu,ld,ru,rd):
+    result = []
+    row_u = len(lu)
+    row_d = len(ld)
+
+    for i in range(row_u):
+        row_temp = lu[i] + ru[i]
+        result.append(row_temp)
+
+    for j in range(row_d):
+        row_temp = ld[j] + rd[j]
+        result.append(row_temp)
+
+    return result
 
 def partitoionMatrix(matrix, startRow, endRow, startCol, endCol):
     result = []
@@ -99,25 +99,7 @@ def strassen_algo(matrixA, matrixB):
         f = partitoionMatrix(matrixB, 1, halfLenOfRow, halfLenOfCol + 1, n)
         g = partitoionMatrix(matrixB, halfLenOfRow + 1, m, 1, halfLenOfCol)
         h = partitoionMatrix(matrixB, halfLenOfRow + 1, m, halfLenOfCol + 1, n)
-        """
-        print("debug:a{} b{} c{} d{}".format(a,b,c,d))
-        print("debug:e{} f{} g{} h{}".format(e,f,g,h))
-        print()
-        print("matrixSubtraction(f,h):",matrixSubtraction(f,h))
-        print("matrixSubtraction(a,b,+):",matrixSubtraction(a,b,"+"))
-        print("matrixSubtraction(c,d,+):",matrixSubtraction(c,d,"+"))
-        print("matrixSubtraction(g,e):",matrixSubtraction(g,e))
-        print("matrixSubtraction(a,d,"+"):",matrixSubtraction(a,d,"+"))
-
-        print(a, matrixSubtraction(f,h) )
-        print(h, matrixSubtraction(a,b,"+") )
-        print(e, matrixSubtraction(c,d,"+") )
-        print(d, matrixSubtraction(g,e) )
-        print( matrixSubtraction(a,d,"+"), matrixSubtraction(e,h,"+") )
-        print( matrixSubtraction(b,d), matrixSubtraction(g,h,"+") )
-        print( matrixSubtraction(a,c), matrixSubtraction(f,e,"+") )
-        print("\n\n")
-        """
+        
         p1 = strassen_algo(a, matrixSubtraction(f,h))
         p2 = strassen_algo(matrixSubtraction(a,b, "+"),h)
         p3 = strassen_algo(matrixSubtraction(c,d, "+"),e)
@@ -126,7 +108,6 @@ def strassen_algo(matrixA, matrixB):
         p6 = strassen_algo(matrixSubtraction(b,d),matrixSubtraction(g,h,"+"))
         p7 = strassen_algo(matrixSubtraction(a,c),matrixSubtraction(e,f,"+"))
 
-        #print("p1{}, p2{}, p3{}, p4{}, p5{}, p6{}, p7{}".format(p1,p2,p3,p4,p5 ,p6 ,p7))
         lu = matrixSubtraction(matrixSubtraction(p5,p4,"+"), matrixSubtraction(p6, p2),"+")
         ru = matrixSubtraction(p1, p2, "+")
         ld = matrixSubtraction(p3, p4, "+")
